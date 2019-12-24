618260eea1c09a046be6e70a704a64be0937bc55
Ash <ashenglandelbro@protonmail.com>
2019-12-24 01:26:52 +0000

    Added signposting.html

--- impact_website/urls.py
+++ impact_website/urls.py
@@ -9,4 +9,5 @@ urlpatterns = (
     path('', views.homepage),
     path('home', views.homepage,  name='impact_website-homepage'),
     path('about-us', views.about_us, name='impact_website-about-us'),
+    path('signposting', views.signposting, name='impact_website-signposting'),
 )
--- impact_website/views.py
+++ impact_website/views.py
@@ -12,3 +12,8 @@ def homepage(request):
 # Handles request for the 'about us' page
 def about_us(request):
     return render(request, 'impact_website/who_are_we.html')
+
+
+# Handles request for the 'signposting' page
+def signposting(request):
+    return render(request, 'impact_website/signposting.html')
