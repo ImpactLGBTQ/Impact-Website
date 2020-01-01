fab068f1cbc82ad340844f438ea6224c276591cf
Ash <ashenglandelbro@protonmail.com>
2020-01-01 22:12:01 +0000

    Added bootstrap intergration

--- ImpactWebsite/settings.py
+++ ImpactWebsite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'impact_website.apps.ImpactWebsiteConfig',
-    'auth_system'
+    'auth_system',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
