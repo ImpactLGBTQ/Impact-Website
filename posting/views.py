25ce051d40e28c1db4f03b72e7a463f00486f1c5
Ash <ashenglandelbro@protonmail.com>
2020-01-02 20:00:35 +0000

    Fixed post returning url instead of response... oops

--- posting/views.py
+++ posting/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.http import response
 from django.views import View
 from . import forms
 from . import models
@@ -28,11 +29,13 @@ class MakeAPostView(View, LoginRequiredMixin):
 
             # Add the post to the database and return
             post = models.Post(author=author, title=title, content=content, image=img)
-            if post.save():
-                # If its a success
-                return reverse_lazy('posting-made-a-post')
+            post.save()
+            # If its a success
+            return response.HttpResponseRedirect(reverse_lazy('posting-made-a-post'))
         # An error occured
+        errors = form.errors.as_data()
         form.add_error(None, "An error occurred during processing of your request")
+        form.add_error(None, errors)
         return render(request, 'posting/make_a_post.html', {'form': form})
 
     ## Handles the get request, displays the 'make a post' page
