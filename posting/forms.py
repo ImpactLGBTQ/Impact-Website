f013edd6de39e20e7da8c5473180ee930978e64b
Ash <ashenglandelbro@protonmail.com>
2020-01-02 18:46:27 +0000

    Styled make a post

--- posting/forms.py
+++ posting/forms.py
@@ -1,5 +1,5 @@
 ## Forms file for the impact website posting app
-from django.forms import forms, ModelForm
+from django.forms import forms, ModelForm, TextInput, Textarea, FileInput
 from .models import Post
 
 
@@ -7,3 +7,12 @@ class MakeAPostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'image')
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'content': Textarea(attrs={'class': 'form-control'}),
+            'image': FileInput(attrs={'class': 'custom-file-input'}),
+        }
+        labels = {
+            'title': 'Title*',
+            'content': '',
+        }
