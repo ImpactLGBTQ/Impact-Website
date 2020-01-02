871fafad2f9a7cd6b6873e4d1db2b07ef35ad843
Ash <ashenglandelbro@protonmail.com>
2020-01-02 18:00:17 +0000

    Added posting app with Post model

---
+++ posting/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from auth_system.models import User
+from ImpactWebsite import settings
+import uuid
+
+
+# Create your models here.
+
+class Post(models.Model):
+    # Post author
+    author = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+
+    # Post uuid
+    uuid = models.UUIDField(default=uuid.uuid4, null=False, primary_key=True)
+
+    # Content and display
+    title = models.CharField(max_length=1024, null=False, default="")
+    content = models.TextField(null=False, default="")
+
+    # Score
+    upvotes = models.IntegerField(null=False, default=0)
+    downvotes = models.IntegerField(null=False, default=0)
+
+    # Optional image
+    image = models.ImageField(default=None)
---
+++ posting/urls.py
@@ -0,0 +1,9 @@
+## Urls file for posting app
+from django import urls
+from django.urls import path
+from . import views
+
+urlpatterns = (
+    path()
+)
+
