9c65238de9b92edd5c0b08feb6e88a12a4d8c808
Ash <ashenglandelbro@protonmail.com>
2020-01-03 01:39:21 +0000

    Fixed styling import issues

--- posting/urls.py
+++ posting/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = (
     path('make-a-post', views.MakeAPostView.as_view(), name='posting-make-a-post'),
     path('made-a-post', views.MakeAPostView.as_view(), name='posting-made-a-post'),
-
+    path('whats-on', views.WhatsOnView.as_view(), name='posting-whats-on'),
 )
 
