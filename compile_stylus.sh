809f7f558547bd501b333fba62f7c5ce67300b7f
Ash <ashenglandelbro@protonmail.com>
2020-01-06 23:12:18 +0000

    Added robots.txt to main branch and updated header bar for new z-value. Also added compile_stylus.sh to compile all stylus files

---
+++ compile_stylus.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+## Sh script for compiling all the stylus files
+stylus static/css
+stylus impact_website/static/impact_website/css
+stylus auth_system/static/auth_system
+stylus posting/static/posting/css
+
+
