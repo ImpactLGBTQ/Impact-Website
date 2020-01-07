b7feb4297d1eb6c08724d541487e4f785fa5cdae
Ash <ashenglandelbro@protonmail.com>
2020-01-07 23:01:29 +0000

    Setup cookie_consent.js

---
+++ static/js/cookie_consent.js
@@ -0,0 +1,23 @@
+/**
+ * Cookie consent js configuration and setup file
+ */
+
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#343c66",
+      "text": "#cfcfe8"
+    },
+    "button": {
+      "background": "transparent",
+      "text": "#f71559",
+      "border": "#f71559"
+    }
+  },
+  "position": "bottom-right",
+  "content": {
+    "message": "This website uses cookies to improve your experience. We do not collect any unnecessary personal data about you",
+    "dismiss": "That's fine",
+    "href": "example.com"
+  }
+});
