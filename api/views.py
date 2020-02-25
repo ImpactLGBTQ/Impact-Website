116a9d8cd030521cf6e68ba41df8bc439560b8ca
Ash <ashenglandelbro@protonmail.com>
2020-02-25 16:47:43 +0000

    Added route to get posts from the API, setup GetPosts view

--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.views import LogoutView
+from django.core.cache import cache
 from django.shortcuts import render
 from django.views import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from .serializers import UserSerializer
+import posting.models as posting_models
+from .serializers import UserSerializer, PostSerializer
 from rest_framework import permissions
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
@@ -47,11 +48,39 @@ class AuthenticateUser(ObtainAuthToken):
 
 class GetPosts(APIView):
 
-    def post(self, request):
-        # Extract the data
-        raw = request.body
-        # Unmarshal it
-        json_data = json.loads(raw)
+    def get(self, request, num):
+        # Ensure not too big
+        if num < 1 or num > 20:
+            return http.HttpResponseBadRequest()
+
+        if request.user.is_anonymous:
+            # If its an annoymous user, aka not logged in person, set the default 0 access level
+            access_level = 0
+        else:
+            # Else use the users access level
+            # Ensure access level is updated
+            request.user.sync_access_level()
+            access_level = request.user.access_level
+        cache_query = 'whats_on_recent-{}'.format(access_level)
+        # Check the cache for these posts
+        cached_posts = cache.get(cache_query)
+        if cached_posts:
+            posts = cached_posts
+            logging.info('Hitting cache for Whats on posts at access level {}'.format(access_level))
+        else:
+            # Pull first 10 posts
+            posts = posting_models.Post.objects.filter(required_access__lte=access_level,
+                                                       post_type__exact=0).order_by()[:10]
+            # Cache the posts
+            cache.set(cache_query, posts)
+            logging.info("Hitting database and adding to cache for Whats on posts at access level {}".format(access_level))
+
+        serialized = PostSerializer(posts)
+        return http.JsonResponse(serialized)
+
+
+
+
 
 
 
